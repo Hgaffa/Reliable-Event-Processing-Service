e7e22377ef573d688f046fc386757d1170e785b1
Hgaffa <hgaffa@gmail.com>
2026-02-03 15:25:07 +0000

    add pytest workflow

--- app/db.py
+++ app/db.py
@@ -1,3 +1,6 @@
+"""
+Docstring for app.db
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,12 +12,12 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SESSIONLOCAL = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
 def get_db():
-    db = SessionLocal()
+    db = SESSIONLOCAL()
     try:
         yield db
     finally:
